016efc0a665712c08dedf17a5e41d2066dbfbb9b
Joseph T <jdtoronto999@outlook.com>
2026-05-13 17:39:23 -0700

    Basic subtask functionality

--- js/main.js
+++ js/main.js
@@ -36,11 +36,11 @@ let totalTasks = 2;
 
 // Array of task objects
 let taskList = [
-    {id: 1, desc: "Do something smart", due:"1987-12-12", complete: false},
+    {id: 1, desc: "Do something smart", due:"1987-12-12", complete: false, subtasks: []},
     {id: 2, desc: "Do something stupid", due:"1999-04-26", complete: true,
         subtasks: [
-            {id: 1, desc: "Jump off a bridge or something", complete: true},
-            {id: 2, desc: "Do a flip", complete: false}
+            {desc: "Jump off a bridge or something", complete: true},
+            {desc: "Do a flip", complete: false}
         ]
     }
 ];
@@ -61,78 +61,120 @@ function loadList(taskList) {
     displayTask(storedTasks);
 }
 
-function displayTask(tasks, isSubtask = false) {
+function displayTask(tasks) {
     // Check if the tasks argument is both an array and not empty
     if (Array.isArray(tasks) && tasks.length > 0) {
         // Shift the first element from the tasks list and return as currentId
         const currentId = tasks.shift();
-        
-        if (!isSubtask) {
-            // Find the current task in the tasks list through the current ID
-            const currentTask = taskList.find(item => item && item.id === currentId);
-            // Find HTML main
-            const taskHolder = document.querySelector("main");
+        // Find the current task in the tasks list through the current ID
+        const currentTask = taskList.find(item => item && item.id === currentId);
+        // Find HTML main
+        const taskHolder = document.querySelector("main");
 
-            // Make new task section
-            const taskSection = document.createElement("section");
-            taskSection.className = "task";
-            taskHolder.appendChild(taskSection);
+        // Make new task section
+        const taskSection = document.createElement("section");
+        taskSection.className = "task";
+        taskHolder.appendChild(taskSection);
 
-            // Make new task complete check button, writing an X if complete
-            const taskButton = document.createElement("button");
-            taskButton.className = "task-button";
-            if (currentTask.complete) {
-                taskButton.textContent = "X";
-            }
-            taskButton.addEventListener("click", () => markComplete(currentTask));
-            taskSection.appendChild(taskButton);
+        // Make new task complete check button, writing an X if complete
+        const taskButton = document.createElement("button");
+        taskButton.className = "task-button";
+        if (currentTask.complete) {
+            taskButton.textContent = "X";
+        }
+        taskButton.addEventListener("click", () => markComplete(currentTask));
+        taskSection.appendChild(taskButton);
 
-            // Make new task description
-            const taskDesc = document.createElement("p");
-            taskDesc.className = "task-desc";
-            taskDesc.textContent = currentTask.desc;
-            taskSection.appendChild(taskDesc);
+        // Make new task description
+        const taskDesc = document.createElement("p");
+        taskDesc.className = "task-desc";
+        taskDesc.textContent = currentTask.desc;
+        taskSection.appendChild(taskDesc);
 
-            // Make new task misc div
-            const taskMisc = document.createElement("div");
-            taskMisc.className = "task-misc";
-            taskSection.appendChild(taskMisc);
+        // Make new task misc div
+        const taskMisc = document.createElement("div");
+        taskMisc.className = "task-misc";
+        taskSection.appendChild(taskMisc);
 
-            // Make new due date element
-            const taskDue = document.createElement("p");
-            taskDue.className = "task-due";
-            // Format due date with Day.js
-            const dueDate = dayjs(currentTask.due);
-            const formattedDue = dueDate.format("M/D/YYYY");
-            // If due date has been passed, mark late
-            if (dueDate.isBefore(now) && !currentTask.complete) {
-                taskDue.classList.add("late-date");
-            }
-            taskDue.textContent = formattedDue;
-            taskMisc.appendChild(taskDue);
+        // Make new due date element
+        const taskDue = document.createElement("p");
+        taskDue.className = "task-due";
+        // Format due date with Day.js
+        const dueDate = dayjs(currentTask.due);
+        const formattedDue = dueDate.format("M/D/YYYY");
+        // If due date has been passed, mark late
+        if (dueDate.isBefore(now) && !currentTask.complete) {
+            taskDue.classList.add("late-date");
+        }
+        taskDue.textContent = formattedDue;
+        taskMisc.appendChild(taskDue);
 
-            // Make new delete button
-            const taskDelete = document.createElement("button");
-            taskDelete.className = "task-delete";
-            taskDelete.textContent = "Delete Task";
-            taskDelete.addEventListener("click", () => deleteTask(currentTask, taskSection));
-            taskMisc.appendChild(taskDelete);
+        // Make new delete button
+        const taskDelete = document.createElement("button");
+        taskDelete.className = "task-delete";
+        taskDelete.textContent = "Delete Task";
+        taskDelete.addEventListener("click", () => deleteTask(currentTask, taskSection, taskList));
+        taskMisc.appendChild(taskDelete);
 
-            // TODO: Make subtask form
+        // Make subtask form
+        const subtaskForm = document.createElement("form");
+        subtaskForm.className = "subtask-form";
+        subtaskForm.id = `subtask-input-${currentTask.id}`;
+        subtaskForm.innerHTML += '<input type="text" name="subtask-text" placeholder="Type in any subtask here!" class="subtask-text" required>';
+        subtaskForm.innerHTML += '<input type="submit" value="Submit Subtask">';
+        taskSection.appendChild(subtaskForm);
+        subtaskForm.onsubmit = () => {
+            // Keep the form from reloading the page and add the subtask
+            event.preventDefault();
+            addSubtask(currentTask, subtaskForm, subtaskHolder);
+        }
 
-            if ("subtasks" in currentTask){
-                // TODO: Make subtask holder, call displayTask for subtasks
-                console.log(currentTask.subtasks);
-            }
-        } else {
-            // TODO: Make subtask display calls
+        // Make subtask holder
+            const subtaskHolder = document.createElement("section");
+            subtaskHolder.className = "subtask-holder";
+            taskSection.appendChild(subtaskHolder);
 
-            console.log("This is a subtask.");
+        // If there are subtasks in the list, display them
+        if (currentTask.subtasks.length > 0){
+            currentTask.subtasks.forEach(currentSubtask => {
+                displaySubtask(currentTask, subtaskHolder, currentSubtask);
+            });
         }
 
         // Recurse until array is empty
-        displayTask(tasks, isSubtask);
+        displayTask(tasks);
     }
+}
+
+function displaySubtask(currentTask, subtaskHolder, currentSubtask) {
+    // Add subtask div
+    const subtaskDiv = document.createElement("div");
+    subtaskDiv.className = "subtask";
+    subtaskHolder.appendChild(subtaskDiv);
+
+    // Add subtask complete check button, writing an X if complete
+    const subtaskButton = document.createElement("button");
+    subtaskButton.className = "subtask-button";
+    if (currentSubtask.complete) {
+        subtaskButton.textContent = "X";
+    }
+    subtaskButton.addEventListener("click", () => markComplete(currentSubtask));
+    subtaskDiv.appendChild(subtaskButton);
+
+    // Add subtask description
+    const subtaskDesc = document.createElement("p");
+    subtaskDesc.className = "subtask-desc";
+    subtaskDesc.textContent = currentSubtask.desc;
+    subtaskDiv.appendChild(subtaskDesc);
+    
+    // Add subtask delete button
+    const subtaskDelete = document.createElement("button");
+    subtaskDelete.className = "subtask-delete";
+    subtaskDelete.textContent = "Delete Subtask";
+    subtaskDelete.addEventListener("click", () => {
+        deleteTask(currentSubtask, subtaskDiv, currentTask.subtasks);
+    });
+    subtaskDiv.appendChild(subtaskDelete);
 }
 
 function markComplete(task) {
@@ -150,11 +192,11 @@ function markComplete(task) {
     saveList();
 }
 
-function deleteTask(task, eventParent) {
+function deleteTask(task, eventParent, listUsed) {
     // Remove task from list
-    const taskIndex = taskList.indexOf(task);
+    const taskIndex = listUsed.indexOf(task);
     if (taskIndex !== -1) {
-        taskList.splice(taskIndex, 1);
+        listUsed.splice(taskIndex, 1);
     }
     
     // Remove task from display
@@ -169,10 +211,10 @@ function addTask() {
     event.preventDefault();
     totalTasks++;
 
-    // Grab what was put on the form as a variable and put it in a new form object
+    // Grab what was put on the form as a variable and put it in a new task object
     const taskDesc = event.target["task-text"].value;
     const taskDue = event.target["task-date"].value;
-    const newTask = {id: totalTasks, desc: taskDesc, due: taskDue, complete: false};
+    const newTask = {id: totalTasks, desc: taskDesc, due: taskDue, complete: false, subtasks: []};
 
     // Push the new task into the task list and add the task to the display
     taskList.push(newTask);
@@ -180,6 +222,22 @@ function addTask() {
 
     // Reset the form and save changes to localStorage
     taskForm.reset();
+    saveList();
+}
+
+function addSubtask(task, form, subtaskHolder) {
+    // Grab the task description from the form and put it in a new subtask object
+    const subtaskDesc = event.target["subtask-text"].value;
+    const newSubtask = {desc: subtaskDesc, complete: false};
+
+    // Push the new subtask into the subtask list
+    task.subtasks.push(newSubtask);
+    
+    // Create a new subtask for the display
+    displaySubtask(task, subtaskHolder, newSubtask);
+
+    // Reset the form and save changes to localStorage
+    form.reset();
     saveList();
 }
 
